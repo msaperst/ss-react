445f22d2e677d834ca2cda73cb0cf6f3f51019fc
Max Saperstone <msaperst@gmail.com>
2021-03-02 15:08:21 -0500

    Adding in coverage checks; Cleaning up auto-generated code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/navbar/Navbar';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     // <NavbarSS list={{header1: ['x', 'y', 'z'], header2: [1, 2, 3]}}/>,
     <Header user={null} section="main"/>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
